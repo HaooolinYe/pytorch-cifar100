4f9d219d1dfd490cdc71e7bff118a1006aa25051
Haolin <90864900+HaooolinYe@users.noreply.github.com>
2023-05-15 16:25:23 -0400

    Update resnet.py

--- models/resnet.py
+++ models/resnet.py
@@ -31,7 +31,7 @@ class BasicBlock(nn.Module):
 #             nn.BatchNorm2d(out_channels),
             nn.ReLU(inplace=True),
             nn.Conv2d(out_channels, out_channels * BasicBlock.expansion, kernel_size=3, padding=1, bias=False),
-            nn.BatchNorm2d(out_channels * BasicBlock.expansion)
+#             nn.BatchNorm2d(out_channels * BasicBlock.expansion)
         )
 
         #shortcut
